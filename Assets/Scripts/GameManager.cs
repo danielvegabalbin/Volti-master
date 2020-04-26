79e1b295ef770b5323f3f2a1dda1fc00f8689d54
danielvegabalbin <daniel-vegabalbin@hotmail.com>
2020-04-26 17:11:20 -0500

    FinalBeta
    Muchos Cambios tanto en ui como otras

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static int PickUpCount;
+
+    void Awake()
+    {
+        PickUpCount = 0;
+    }
+
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect((Screen.width / 2.0f), 100, 250, 400), string.Format("{0}", PickUpCount));
+    }
+
+}
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    public int CoinValue;
+    private bool triggered_;
+ //   private AudioSource audio_;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggered_ )
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter()
+    {
+        triggered_ = true;
+        // audio_.enabled = true;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        GameManager.PickUpCount += CoinValue;
+    }
+}
