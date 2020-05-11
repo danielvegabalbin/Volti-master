cce69a9df60e0a7ad406351ddbb68edbe4994ec6
danielvegabalbin <daniel-vegabalbin@hotmail.com>
2020-05-11 11:26:41 -0500

    CAMBIOS 1
    pequeños cambios en el juego

---
+++ Assets/Scripts/PauseBTN.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseBTN : MonoBehaviour
+{
+
+    bool isPaused = false;
+
+
+    public void pausedGame()
+    {
+
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+
+    }
+    
+    
+    
+}
